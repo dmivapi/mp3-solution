69aa1fc436ca9efb5074d47898007ccc115ff3b3
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-03-13 17:56:24 +0200

    Fixing ParsingIdException message.

--- song-service/src/main/java/com/epam/learn/song/exception/ParsingIdException.java
+++ song-service/src/main/java/com/epam/learn/song/exception/ParsingIdException.java
@@ -2,7 +2,7 @@ package com.epam.learn.song.exception;
 
 public class ParsingIdException extends RuntimeException {
 
-    private static final String ERROR_MESSAGE_TEMPLATE = "Provided ID=%s is not a positive number";
+    private static final String ERROR_MESSAGE_TEMPLATE = "Invalid ID format: '%s'. Only positive integers are allowed";
 
     public ParsingIdException(String id) {
         super(ERROR_MESSAGE_TEMPLATE.formatted(id));

0dd5da471c8800b84c90a677ab6dddeab779764d
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-03-19 00:54:12 +0200

    Bug fixing.

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 }
 
--- resource-service/src/main/java/com/epam/learn/resource/config/ClientConfig.java
+++ resource-service/src/main/java/com/epam/learn/resource/config/ClientConfig.java
@@ -3,8 +3,10 @@ package com.epam.learn.resource.config;
 import com.epam.learn.resource.config.properties.SongClientProperties;
 import com.epam.learn.song.client.SongsApi;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,9 +14,22 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 @EnableConfigurationProperties(SongClientProperties.class)
 public class ClientConfig {
+
     @Bean
-    public RestClient songsRestClient(SongClientProperties properties) {
-        return RestClient.builder()
+    @Primary
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public RestClient songsRestClient(@LoadBalanced RestClient.Builder loadBalancedRestClientBuilder, SongClientProperties properties) {
+        return loadBalancedRestClientBuilder
                 .baseUrl(properties.baseUrl())
                 .build();
     }

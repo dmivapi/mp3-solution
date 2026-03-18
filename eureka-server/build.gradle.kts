86473bdffd7228eb9afe94dfe2573686f4840df3
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-03-18 23:17:39 +0200

    Added eureka server and related changes.

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.epam.learn"
+version = "0.0.1-SNAPSHOT"
+description = "eureka-server"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.1"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "mp3-solution"
 include("resource-service")
 include("song-service")
+include("eureka-server")

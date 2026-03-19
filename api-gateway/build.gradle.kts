614768a5db6bda0d2d0d10c2351397fa2b911604
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-03-19 14:32:53 +0200

    Adding API Gateway.

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.epam.learn"
+version = "0.0.1-SNAPSHOT"
+description = "api-gateway"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(libs.spring.cloud.bom.get().toString())
+    }
+}
---
+++ api-gateway/src/main/java/com/epam/learn/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.epam.learn.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ApiGatewayApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
